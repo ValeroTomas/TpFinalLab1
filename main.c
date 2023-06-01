e76eb0f736350ab8a878348065b980559e17126c
ValeroTomas <128714032+ValeroTomas@users.noreply.github.com>
2023-06-01 11:54:54 -0300

    main V0.03.0.c
    - Creada estructura stProducto
    - Agregados comentarios para la comodidad de la lectura
    - Agregada función principal stProducto nuevoProducto();
    - Agregada función de carga de producto cargarProducto();
    - Agregada función de leer y mostrar producto.
    
    **Falta comprobar correcto funcionamiento de las id**
    **Falta optimizar el llamado a las funciones en el Switch/Case (2 y 3)**

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <windows.h>
 
+/** COLORES PARA LA TERMINAL DE WINDOWS*/
 #define ROJO     12
 #define VERDE   10
 #define AMARILLO  14
@@ -9,12 +10,28 @@
 #define CYAN    11
 #define BLANCO   15
 
+/** ESTRUCTURA DE PRODUCTO*/
+typedef struct
+{
+    int id;
+    char nombre [50];
+    int existencias;
+    int precio;
+} stProducto;
+
+/** FUNCIONES*/
+stProducto nuevoProducto(int i);
 void color(int color);
 void menu();
 void verificarArchivo(char archivo[]);
+void cargarProducto(FILE *listadoFile, char listado[]);
+void leerArchivo(FILE *listadoFile, char listado[]);
 
-const char tpFinal[] = "Gestor de Stock V0.02";
+/** NOMBRE Y VERSIÓN DEL PROGRAMA*/
+const char tpFinal[] = "Gestor de Stock V0.03.0";
 
+
+/** FUNCIÓN PRINCIPAL*/
 int main()
 {
     int opc;
@@ -35,12 +52,19 @@ int main()
         switch(opc)
         {
         case 1:
+            system("cls");
+            leerArchivo(listado, listadoFile);
+            system("PAUSE");
+            system("cls");
             break;
 
         case 2:
             break;
 
         case 3:
+            system("cls");
+            cargarProducto(listado, listadoFile);
+            system("cls");
             break;
 
         case 4:
@@ -75,6 +99,106 @@ int main()
     return 0;
 }
 
+/** FUNCIÓN BASE DE CARGA DE PRODUCTO*/
+stProducto nuevoProducto(int i)
+{
+    stProducto producto;
+    int id = i + 1;
+
+    producto.id = id;
+    color(CYAN);
+    printf("\nIngrese el nombre.\n");
+    color(BLANCO);
+    fflush(stdin);
+    gets(producto.nombre);
+    color(CYAN);
+    printf("\nIngrese numero de existencias:\n");
+    color(BLANCO);
+    fflush(stdin);
+    scanf("%d", &producto.existencias);
+    color(CYAN);
+    printf("\nIngrese precio (SIN SIGNO '$'):\n");
+    color(BLANCO);
+    fflush(stdin);
+    scanf("%d", &producto.precio);
+    return producto;
+}
+
+/** FUNCIÓN PARA CARGAR EL PRODUCTO AL ARCHIVO*/
+void cargarProducto(FILE *listadoFile, char listado[])
+{
+    stProducto producto;
+    stProducto cache;
+    listadoFile = fopen(listado, "rb");
+    int i = 0;
+    int size;
+    if(listadoFile != NULL)
+    {
+        fseek(listadoFile, 0, SEEK_END);
+        size = ftell(listadoFile);
+        if(size > 0)
+        {
+            fseek(listadoFile, -sizeof(stProducto), SEEK_END);
+            fread(&cache, sizeof(stProducto),1, listadoFile);
+            i = cache.id;
+            printf("%d", i);
+        }
+        fclose(listadoFile);
+        listadoFile = fopen(listado, "ab");
+        producto = nuevoProducto(i);
+        fwrite(&producto, sizeof(stProducto), 1, listadoFile);
+        fclose(listadoFile);
+    }
+}
+
+/** FUNCIÓN PARA MOSTRAR ARCHIVO EN LA TERMINAL*/
+void leerArchivo(FILE *listadoFile, char listado[])
+{
+
+    stProducto producto;
+    listadoFile = fopen(listado, "rb");
+    if (listadoFile!= NULL)
+    {
+        while(!feof(listadoFile))
+        {
+            fread(&producto, sizeof(stProducto),1, listadoFile);
+            if(!feof(listadoFile))
+            {
+                color(AMARILLO);
+                printf("\n______________________________________\n\n");
+                color(CYAN);
+                printf ("ID: ");
+                color(BLANCO);
+                printf ("%i\n", producto.id);
+                color(CYAN);
+                printf ("NOMBRE: ");
+                color(BLANCO);
+                printf ("%s\n", producto.nombre);
+                color(CYAN);
+                printf ("EXISTENCIAS: ");
+                color(BLANCO);
+                printf ("%i\n", producto.existencias);
+                color(CYAN);
+                printf ("PRECIO: ", producto.precio);
+                color(BLANCO);
+                printf ("$%i\n", producto.precio);
+                color(AMARILLO);
+                printf("______________________________________\n");
+                color(BLANCO);
+            }
+            printf("\n");
+        }
+        fclose(listadoFile);
+    }
+    else
+    {
+        printf ("EL ARCHIVO NO EXISTE\n");
+    }
+
+
+}
+
+/** MENÚ PRINCIPAL DEL PROGRAMA*/
 void menu()
 {
     color(CYAN);
@@ -83,7 +207,7 @@ void menu()
     printf(" |                                                               |\n");
     printf(" |");
     color(BLANCO);
-    printf("                     %s                     ", tpFinal);
+    printf("                    %s                    ", tpFinal);
     color(CYAN);
     printf("|\n");
     printf(" |                                                               |\n");
@@ -131,12 +255,14 @@ void menu()
     color(BLANCO);
 }
 
+/** FUNCIÓN DE COLORES EN TERMINAL DE WINDOWS*/
 void color(int color)
 {
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleTextAttribute(hConsole, color);
 }
 
+/** VERIFICACIÓN DE EXISTENCIA DE ARCHIVO*/
 void verificarArchivo(char archivo[])
 {
     FILE *arch;
@@ -191,3 +317,4 @@ void verificarArchivo(char archivo[])
         }
     }
 }
+
